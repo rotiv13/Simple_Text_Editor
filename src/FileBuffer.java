924ac0a392c1c2fbfc1b425f127c5b0b6cf3593c
Vitor <up200908303@alunos.dcc.fc.up.pt>
2015-05-11 13:07:29 +0100

    Started implementing FileBuffer.java

--- src/FileBuffer.java
+++ src/FileBuffer.java
@@ -1,2 +1,9 @@
-public class FileBuffer {
+public class FileBuffer extends Buffer {
+    boolean modified = false;
+
+    @Override
+    public void insertChar(char c) {
+        super.insertChar(c);
+        modified = true;
+    }
 }
